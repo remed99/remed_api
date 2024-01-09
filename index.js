973ea73c5ae3dcf7eecf211f922e79da13da2f64
aashu79 <aashusah435@gmail.com>
2024-01-09 20:55:10 +0545

    added new pacakge morgan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
+const morgan = require("morgan");
 
 //Module imports
 const connect = require("./utilis/dbConnection");
@@ -16,6 +17,7 @@ const homeRoute = require("./routes/home.route");
 //middelwares
 app.use(express.json());
 app.use(cors());
+app.use(morgan('common'));
 
 //Routes
 app.use('/', homeRoute );
